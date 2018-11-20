c04aa65a87058e206717fc26a8ac11b7631f02fd
FernandoVazquezz <luisfernando.flores@alumno.uttehuacan.edu.mx>
2018-11-20 15:16:51 -0600

    Save-MuestraenList

--- restauratApp/src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ restauratApp/src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -89,4 +89,3 @@ export class RecipesEditComponent implements OnInit {
     this.editMode = false;
   }
 }
-
--- restauratApp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ restauratApp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Recipe } from '../recipe.model';
 
-@Component({
+@Component({ 
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
   recipes:Recipe[];
+  private sure:Subscription;
+  constructor(private recipeService:RecipeService, private router:Router, private route: ActivatedRoute) {
 
-  constructor(private recipeService: RecipeService,private router:Router,private route:ActivatedRoute) { }
-
+   }
+ 
+  onNewRecipe(){
+    this.router.navigate(['new'],{relativeTo:this.route});
+   }
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+    this.recipeService.recipeChanged.subscribe((event)=>{
+      this.recipes = this.recipeService.getRecipes();
+    })
   }
 
-  onNewRecipe() {
-    this.router.navigate(['new'],{relativeTo: this.route});
+
+  ngOnDestroy(){
+    this.sure.unsubscribe();
   }
 
 }
